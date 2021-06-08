e4eea1560b147513c4b5492df069a9a8de7a2407
Vitalii Oliinyk <vitaliyoliynykk@gmail.com>
2021-06-08 11:30:33 +0300

    player animation and rotation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'phaser';
 import sky from '../assets/sky.jpeg';
 import ground from '../assets/ground.jpeg';
 import obstacle from '../assets/obstacle.jpeg';
+import bird from '../assets/bird.png';
 
 const config = {
     width: 400,
@@ -14,7 +15,7 @@ const config = {
       default: 'arcade',
       arcade: {
         fps: 60,
-        gravity: {y : 0},
+        gravity: {y : 400},
       }
     },
 };
@@ -23,7 +24,10 @@ const game = new Phaser.Game(config);
 
 class MainScene extends Phaser.Scene {
     private ground: Phaser.Physics.Arcade.StaticGroup;
-    private obstacles: Phaser.Physics.Arcade.Group[];
+    private obstacles: Phaser.Physics.Arcade.StaticGroup[];
+    private player: Phaser.Physics.Arcade.Sprite;
+    private keyboard: Phaser.Types.Input.Keyboard.CursorKeys;
+    private initialRotation = 0;
 
     constructor() {
         super('MainScene');
@@ -31,10 +35,19 @@ class MainScene extends Phaser.Scene {
 
      public preload(): void{
       this.loadAssets();
+      this.keyboard = this.input.keyboard.createCursorKeys();
     }
 
      public create(): void {
         this.add.image(200, 300, 'sky').setScale(1.4);
+        this.player = this.physics.add.sprite(200, 300, 'bird').setScale(2.4);
+        this.anims.create({
+          key: 'fly',
+          frames: this.anims.generateFrameNumbers('bird', {start: 0, end: 2}),
+          frameRate: 10,
+          repeat: -1,
+        })
+        this.player.anims.play('fly');
         this.createGround();
         this.createObstacles();
     }
@@ -42,12 +55,33 @@ class MainScene extends Phaser.Scene {
     public update(): void {
       this.moveBackground();
       this.moveObstacles();
+      this.handleControl();
+    }
+
+    private handleControl(): void {
+      if(this.keyboard.space.isDown) {
+        if(this.player.body.velocity.y >= - 240) {
+          this.player.setVelocityY(this.player.body.velocity.y - 80);
+          if(this.initialRotation >= -1) {
+            this.setPlayerRotation();
+          }
+        }
+      }
+      if(this.initialRotation <= 1 && this.keyboard.space.isUp) {
+        this.setPlayerRotation();
+      }
+    }
+
+    private setPlayerRotation(): void {
+      this.initialRotation = this.player.body.velocity.y * 0.003 
+      this.player.setRotation(this.initialRotation);
     }
 
     private loadAssets(): void {
       this.load.image('sky', sky);
       this.load.image('ground', ground);
       this.load.image('obstacle', obstacle);
+      this.load.spritesheet('bird', bird, {frameWidth: 18, frameHeight: 18, spacing: 11});
     }
 
     private createGround() {
@@ -68,11 +102,11 @@ class MainScene extends Phaser.Scene {
     }
 
     private createObstacles(): void {
-      this.obstacles = [this.physics.add.group(), this.physics.add.group()];
+      this.obstacles = [this.physics.add.staticGroup(), this.physics.add.staticGroup()];
       this.obstacles.forEach(this.configureObstacle);
     }
 
-    private configureObstacle(obstacle: Phaser.Physics.Arcade.Group, obstacleIndex: number): void {
+    private configureObstacle(obstacle: Phaser.Physics.Arcade.StaticGroup, obstacleIndex: number): void {
       const startOfGap = Math.floor(Math.random() * 7) + 2;
 
         for (let i = 0; i < 10; i++) {
